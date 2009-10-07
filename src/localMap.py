7841f7efca9a05d8baab3ccb78d2529163c03938
David Christopher Pursell <dpursell@myth13.stanford.edu>
2009-10-06 18:14:19 -0700

    Added line intersection code to ransac.py and began localMap.py

---
+++ src/localMap.py
@@ -0,0 +1,26 @@
+from ransac import LineModel
+from math import *
+
+class Wall:
+  def __init__(self, line):
+    self.lineModel = line
+    self.polar = [line.distanceToPoint([0,0]), atan2(line.trajectory[1], line.trajectory[0])]
+
+class LocalMap:
+  def __init__(self):
+    self.walls = []
+    self.corners = []
+    self.minAngleDiff = 5.0 * pi / 180.0
+    self.minDistDiff = 0.4
+  def wallIs(self, w):
+    if not wallExists(w):
+      walls.push(Wall(w))
+      for wall in self.walls:
+        isect = wall.intersection(w)
+        if isect is not None:
+          self.corners.push(wall.intersection(w))
+  def wallExists(self, w):
+    for wall in self.walls:
+      if wall.angleBetween(w) < self.minAngleDiff and wall.distanceToPoint(w.origin) < self.minDistDiff:
+        return True
+    return False
