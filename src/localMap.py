3d6d1ad41282ec84786649157b253ac392d5fc85
David Christopher Pursell <dpursell@myth2.stanford.edu>
2009-10-06 21:17:09 -0700

    localMap now enforces rectilinearity

--- src/localMap.py
+++ src/localMap.py
@@ -4,34 +4,42 @@ import math
 class Wall:
   def __init__(self, line):
     self.lineModel = line
-    self.polar = [line.distanceToPoint([0,0]), atan2(line.trajectory[1], line.trajectory[0])]
+    self.polar = [line.distanceToPoint([0,0]), math.atan2(line.trajectory[1], line.trajectory[0])]
+  def angleBetween(self, line):
+    return self.lineModel.angleBetween(line)
+  def origin(self):
+    return self.lineModel.origin
+  def trajectory(self):
+    return self.lineModel.trajectory
 
 class LocalMap:
   def __init__(self):
     self.walls = []  # list of walls, each element is of the format [wall, confidence]
     self.maxAngleDiff = 5.0 * math.pi / 180.0  # the maximum degree difference to be the same wall
     self.maxDistDiff = 0.4  # the maximum distance apart to be the same wall
-    self.rectilinearMin = math.pi / 2.0 - self.MaxAngleDiff  # minimum angle to be rectilinear
-    self.rectilinearMax = math.pi / 2.0 + self.MaxAngleDiff
 
-  def wallIs(self, w):
-    i = self.wallIndex(w)  # returns an index if close enough, -1 otherwise
+  def wallIs(self, line):
+    i = self.wallIndex(line)  # returns an index if close enough, -1 otherwise
     if i < 0:  # no similar wall found
-      if self.isRectilinear(w):  # this one will work
-        walls.push([Wall(w), 1])
+      if self.isRectilinear(line):  # this one will work
+        self.walls.append([Wall(line), 1])
     else:  # similar wall was found
-      walls[i][1] += 1  # add confidence value
+      self.walls[i][1] += 1  # add confidence value
 
-  def wallIndex(self, w):
+  def wallIndex(self, line):
     for wall in self.walls:
-      if wall.angleBetween(w) < self.maxAngleDiff and wall.distanceToPoint(w.origin) < self.maxDistDiff:
+      if wall[0].angleBetween(line) < self.maxAngleDiff and wall[0].distanceToPoint(line.origin) < self.maxDistDiff:
         return self.walls.index(wall)
     return -1
 
-  def isRectilinear(self, w):
+  def isRectilinear(self, line):
+    if len(self.walls) == 0:
+      return True
+    angleTests = [0, math.pi/2.0, math.pi, 3.0*math.pi/2.0, 2*math.pi]
     for wall in self.walls:
-      a = wall.angleBetween(w)
-      if a > self.rectilinearMin and a < self.rectilinearMax:
-        return True
+      a = wall[0].angleBetween(line)
+      for test in angleTests:
+        if a > test-self.maxAngleDiff and a < test+self.maxAngleDiff:
+          return True
     return False
             
